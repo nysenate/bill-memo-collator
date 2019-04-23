3f55e37d6f66576c388382c861ea4aab744ff8cc
Ken Zalewski <zalewski.senate@gmail.com>
2019-04-22 20:12:50 -0400

    Initial commit.

---
+++ collate_bill_memos.sh
@@ -0,0 +1,152 @@
+#!/bin/sh
+#
+# collate_bill_memos.sh - Given an input PDF with XML placeholder tags,
+#   transform the PDF by replacing the pages containing XML tags with
+#   the support/opposition memo PDFs that those tags reference.
+#
+# Author: Ken Zalewski
+# Organization: New York State Senate
+# Date: 2019-03-01
+# Revised: 2019-03-06
+#
+
+prog=`basename $0`
+memodir="/minjdriv/MEMOS ON FILE/Memo PDFs/2019 - 20 Memos on File"
+tmpdir="/tmp/collate_bill_memos.tmp.d"
+keep_tmpdir=0
+srcfile=
+outfile=
+
+if [ "$OSTYPE" = "cygwin" ]; then
+  tmpdir="C:/cygwin$tmpdir"
+  memodir="Z:$memodir"
+else
+  memodir="/mnt$memodir"
+fi
+
+set -o pipefail
+
+usage() {
+  echo "Usage: $prog [--dir memodir] [--keep-tmpdir] [--tmpdir tempdir] [--output-file collated_file.pdf] lbdc_source_file.pdf" >&2
+}
+
+cleanup() {
+  rm -f doc_data.txt
+  [ $keep_tmpdir -eq 0 -a -d "$tmpdir" ] && rm -rf "$tmpdir"
+}
+
+cleanup_and_exit() {
+  cleanup
+  [ "$1" ] && exit $1 || exit 2
+}
+
+
+if [ $# -lt 1 ]; then
+  usage
+  exit 1
+fi
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -d|--dir) shift; memodir="$1" ;;
+    -h|--help) usage; exit 0 ;;
+    -k|--keep*) keep_tmpdir=1 ;;
+    -o|--out*) shift; outfile="$1" ;;
+    -t|--tmp*) shift; tmpdir="$1" ;;
+    -*) echo "$prog: $1: Unknown option" >&2; usage; exit 1 ;;
+    *) srcfile="$1" ;;
+  esac
+  shift
+done
+
+if [ ! "$srcfile" ]; then
+  echo "$prog: LBDC source file must be specified" >&2
+  exit 1
+elif [ ! -r "$srcfile" ]; then
+  echo "$prog: $srcfile: LBDC source file not found" >&2
+  exit 1
+elif [ ! -d "$memodir" ]; then
+  echo "$prog: $memodir: PDF memo directory not found" >&2
+  exit 1
+fi
+
+# If no output file was specified, generate one based on the source filename.
+if [ ! "$outfile" ]; then
+  outfile=`basename "$srcfile" .pdf`.collated.pdf
+fi
+
+# Confirm that "pdfgrep" and "pdftk" are both available.
+if ! type -p pdfgrep pdftk >/dev/null; then
+  echo "$prog: Both "pdfgrep" and "pdftk" must be available" >&2
+  exit 1
+fi
+
+# Trap various signals to abort cleanly.
+trap cleanup_and_exit 1 2 3 6 15
+
+# Create the temporary work directory
+mkdir -p "$tmpdir" || exit 1
+
+# Split the source PDF document into multiple single-page documents.
+echo "Splitting [$srcfile] into individual single-page PDF files"
+pdftk "$srcfile" burst output "$tmpdir/page_%03d.pdf"
+
+if [ $? -ne 0 ]; then
+  echo "$prog: Unable to split source PDF [$srcfile] into single-page PDFs" >&2
+  cleanup_and_exit 1
+fi
+
+# Iterate over the individual pages looking for LBDC placeholder tags.
+# Whenever a placeholder tag is encountered, attempt to replace the entire
+# page with the PDF memo that is referenced by the tag.
+
+page_count=0
+memo_count=0
+err_count=0
+
+for f in "$tmpdir"/page_???.pdf; do
+  echo "Processing file [$f]"
+  fname=`basename $f`
+  let page_count++
+  if pdfgrep '<(LBDC_PARM|EXTERNAL_BILL_MEMO) [^>]*/>' "$f" | tr '\n' ' ' > "$tmpdir/curtag.xml"; then
+    echo "Found LBDC placeholder tag in [$fname]:"
+    cat "$tmpdir/curtag.xml"; echo
+    let memo_count++
+    memofname=`sed -nr 's;.* (value|filename)="([^"]*)".*;\2;p' "$tmpdir/curtag.xml"`
+    if [ "$memofname" ]; then
+      echo "Attempting to replace [$fname] with memo file [$memofname]"
+      memofile="$memodir/$memofname"
+      if [ -f "$memofile" -a -r "$memofile" ]; then
+        cp -v "$memofile" "$f"
+      else
+        echo "$prog: Unable to find memo file [$memofile]; [$fname] will not be replaced" >&2
+        let err_count++
+      fi
+    else
+      echo "$prog: Unable to parse memo filename from LBDC placeholder tag" >&2
+      let err_count++
+    fi
+  fi
+done
+
+if [ $err_count -gt 0 ]; then
+  echo "$prog: WARNING: $err_count page(s) could not be replaced" >&2
+fi
+
+# Reassemble all of the individual PDF files into a single PDF file.
+echo "Reassembling the individual PDF files into a single PDF output file"
+pdftk "$tmpdir"/page_???.pdf cat output "$outfile"
+
+if [ $? -eq 0 ]; then
+  echo "Generated output file [$outfile]"
+else
+  echo "$prog: $outfile: Unable to generate output file" >&2
+  cleanup_and_exit 1
+fi
+
+echo "Document summary:"
+echo "Page count:  $page_count"
+echo "Memo count:  $memo_count"
+echo "Error count: $err_count"
+
+cleanup_and_exit $err_count
---
+++ collate_pdf.php
@@ -0,0 +1,252 @@
+<?php
+
+// Some constants for us.
+$output_dir = '/data/billmemos';
+$log_dir = '/var/log/billmemos';
+$log_file = $log_dir.'/collate_pdf.log';
+$exec_script = dirname(__FILE__).'/collate_bill_memos.sh';
+
+if (!empty($_FILES['userfiles']) && !empty($_POST['process'])) {
+  /*
+    Do the processing here.  The loop below demonstrates how
+    the files are enumerated in PHP.
+  */
+  $output_files = [];
+  $messages = [];
+  $success = true;
+  $exec_out = [];   // output from the shell command as an array of lines
+  $exec_ret = 0;    // return code from the shell command
+  $userfiles = $_FILES['userfiles'];
+
+  if (isset($userfiles['name']) && count($userfiles['name']) > 0) {
+    foreach ($userfiles['name'] as $idx => $fname) {
+      if ($fname) {
+        $fsize = $userfiles['size'][$idx];
+        $fpath = $userfiles['tmp_name'][$idx];
+
+        // Messages for debugging.
+        $messages[] = "Received file #".($idx+1).": $fname ($fsize)";
+
+        // Process the files.
+        $out_fname = preg_replace('/[.]pdf$/', '.collated.pdf', $fname);
+        $out_fpath = realpath($output_dir)."/$out_fname";
+        $exec_str = "$exec_script \"$fpath\" --output-file \"$out_fpath\" 2>&1";
+        $exec_out = [];
+
+        exec("$exec_str", $exec_out, $exec_ret);
+
+        // Log the output from the shell script.
+        $exec_out = implode("\n", $exec_out)."\n";
+        file_put_contents($log_file, $exec_out, FILE_APPEND | LOCK_EX);
+
+        if ($exec_ret == 0) {
+          // Add the final processed file location to the output list.
+          $output_files[] = "/collated/$out_fname";
+          $messages[] = "File $fname processed successfully; collated file is $out_fname";
+        }
+        else {
+          $messages[] = "exec() failed; error code=$exec_ret";
+          $messages[] = "Command line:\n$exec_str";
+          $messages[] = "Command output:\n$exec_out";
+        }
+      }
+    }
+  }
+
+  if (!count($output_files)) {
+    $success = false;
+    $messages[] = 'No files received.';
+  }
+
+  $result = (object) [
+    'files' => $output_files,
+    'messages' => $messages,
+    'success' => $success,
+  ];
+
+  $json = json_encode($result);
+  if ($json !== false) {
+    echo $json;
+    exit(0);
+  }
+  else {
+    exit(1);
+  }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PDF Processor</title>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script type="text/javascript">
+    $(function () {
+      (function (jQuery, document, window, undefined) {
+        var isAdvancedUpload = function () {
+          var div = document.createElement('div');
+          return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+        }();
+        var $form = $('.box'), $input = $('.box__file');
+
+        if (isAdvancedUpload) {
+          $form.addClass('has-advanced-upload');
+
+          var droppedFiles = false;
+
+          $form.on('drag dragstart dragend dragover dragenter dragleave drop', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+          })
+            .on('dragover dragenter', function () {
+              $form.addClass('is-dragover');
+            })
+            .on('dragleave dragend drop', function () {
+              $form.removeClass('is-dragover');
+            })
+            .on('drop', function (e) {
+              droppedFiles = e.originalEvent.dataTransfer.files;
+              if (droppedFiles) {
+                $input.hide();
+                var $drag = $('.box__dragged').html('');
+                for (var x=0; x < droppedFiles.length; x++) {
+                  $drag.append('<li>' + droppedFiles.item(x).name + '</li>');
+                }
+              }
+            });
+        }
+
+        $form.on('submit', function (e) {
+          if ($form.hasClass('is-uploading')) {
+            return false;
+          }
+
+          $form.addClass('is-uploading').removeClass('is-error');
+
+          if (isAdvancedUpload) {
+            e.preventDefault();
+
+            var ajaxData = new FormData($form.get(0));
+
+            if (droppedFiles) {
+              ajaxData.delete($input.attr('name'));
+              $.each(droppedFiles, function (i, file) {
+                ajaxData.append($input.attr('name'), file, file.name);
+              });
+            }
+
+            $.ajax({
+              url: $form.attr('action'),
+              type: $form.attr('method'),
+              data: ajaxData,
+              dataType: 'json',
+              cache: false,
+              contentType: false,
+              processData: false,
+              complete: function (d) {
+                $form.removeClass('is-uploading');
+                $r = $('#results').html('');
+                d.responseJSON.files.forEach(function(v,i) {
+                  $r.append('<li><a href="' + v + '">' + v + '</a></li>');
+                });
+                $r.append('<h3>Messages</h3>');
+                d.responseJSON.messages.forEach(function(v,i) {
+                  $r.append('<pre>' + v + '</pre>');
+                });
+              },
+              success: function (data) {
+                $form.addClass(data.success == true ? 'is-success' : 'is-error');
+              },
+              error: function () {
+                // Log the error, show an alert, whatever works for you
+                alert('there was an error');
+              }
+            });
+          }
+          else {
+            var iframeName = 'uploadiframe' + new Date().getTime();
+            $iframe = $('<iframe name="' + iframeName + '" style="display: none;"></iframe>');
+
+            $('body').append($iframe);
+            $form.attr('target', iframeName);
+
+            $iframe.one('load', function () {
+              var data = JSON.parse($iframe.contents().find('body').text());
+              $form
+                .removeClass('is-uploading')
+                .addClass(data.success == true ? 'is-success' : 'is-error')
+                .removeAttr('target');
+              if (!data.success) {
+                $errorMsg.text(data.error);
+              }
+              $form.removeAttr('target');
+              $iframe.remove();
+            });
+          }
+        });
+      })(jQuery, document, window);
+    });
+  </script>
+  <style type="text/css">
+    div {
+      max-width: 1000px;
+    }
+
+    .box__dragndrop,
+    .box__uploading,
+    .box__success,
+    .box__error {
+      display: none;
+    }
+
+    .box {
+      font-size: 1.25rem;
+      background-color: #c8dadf;
+      position: relative;
+      padding: 100px 20px;
+    }
+
+    .box.has-advanced-upload {
+      outline: 2px dashed #92b0b3;
+      outline-offset: -10px;
+      -webkit-transition: outline-offset .15s ease-in-out, background-color .15s linear;
+      transition: outline-offset .15s ease-in-out, background-color .15s linear;
+    }
+
+    .box.has-advanced-upload .box__dragndrop {
+      display: inline;
+    }
+
+    .box.is-dragover {
+      background-color: grey;
+    }
+
+    .box.is-uploading .box__input {
+      visibility: none;
+    }
+
+    .box.is-uploading .box__uploading {
+      display: block;
+    }
+  </style>
+</head>
+<body>
+<div>
+  <form class="box" method="post" action="" enctype="multipart/form-data">
+    <input type="hidden" name="process" value="1"/>
+    <div class="box__input">
+      <input class="box__file" type="file" name="userfiles[]" id="file"
+             data-multiple-caption="{count} files selected" multiple/>
+      <label for="file"><strong>Choose a file</strong><span
+            class="box__dragndrop"> or drag it here.</span></label>
+      <button class="box__button" type="submit">Upload</button>
+      <div class="box__dragged"></div>
+    </div>
+    <div class="box__uploading">Uploading&hellip;</div>
+    <div class="box__success">Done!</div>
+    <div class="box__error">Error! <span></span>.</div>
+  </form>
+  <h1>RESULTS:</h1>
+  <pre id="results"></pre>
+</div>
+</body>
+</html>
